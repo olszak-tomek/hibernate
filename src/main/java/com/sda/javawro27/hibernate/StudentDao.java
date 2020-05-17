8632c4a4adb048499a1a7f3c483d830d162431fd
Tomasz Olszak <olszak.tomek@gmail.com>
2020-05-17 12:56:51 +0200

    poprawione listowanie

--- src/main/java/com/sda/javawro27/hibernate/StudentDao.java
+++ src/main/java/com/sda/javawro27/hibernate/StudentDao.java
@@ -5,21 +5,44 @@ import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.ArrayList;
+import java.util.List;
 
 // umożliwia wykonywanie operacji CRUD na modelu Student
 public class StudentDao {
+
     public void saveOrUpdate(Student student) {
         SessionFactory sessionFactory = HibernateUtil.getOurSessionFactory();
         Transaction transaction = null;
+
         try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
+
             // instrukcja która służy do zapisywania w bazie
             session.saveOrUpdate(student);
+
             transaction.commit();
         } catch (HibernateException he) {
             if (transaction != null) {
                 transaction.rollback();
             }
         }
+    }
+
+    public List<Student> getAll(){
+        List<Student> list = new ArrayList<>();
+
+        SessionFactory sessionFactory = HibernateUtil.getOurSessionFactory();
+        try (Session session = sessionFactory.openSession()) {
+            // h q l = hibernate query language
+            Query<Student> studentQuery = session.createQuery("SELECT a from Student a", Student.class);
+
+            list.addAll(studentQuery.getResultList());
+        } catch (HibernateException he) {
+            he.printStackTrace();
+        }
+        return list;
     }
 }
